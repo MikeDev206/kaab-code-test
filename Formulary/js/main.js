78162d23ff0f85cd6fab6cff22da10d2fea27e3f
Miguel Angel Aceves <acevesm77@gmail.com>
2020-03-21 11:37:57 -0600

    funcion de ocultar un formulario

---
+++ Formulary/js/main.js
@@ -0,0 +1,9 @@
+$('.toggle').click(function(){
+  $('.form').animate({
+    height: "toggle",
+    'padding-top': "toggle",
+    'padding-bottom': "toggle",
+    opacity: "toggle"
+
+  }, "slow")
+})
